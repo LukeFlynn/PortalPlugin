93c74ace31f0a02ca6530e77f04afadc7a7cc5ff
Luke Flynn <luke@qixalite.com>
2017-02-07 01:09:20 -0500

    Initial Commit

---
+++ src/main/java/com/epochmc/portalpluginlegacy/PortalPlugin.java
@@ -0,0 +1,41 @@
+package com.epochmc.portalpluginlegacy;
+
+import java.util.HashMap;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.configuration.file.FileConfiguration;
+
+public class PortalPlugin extends JavaPlugin {
+    public FileConfiguration config;
+    private final PortalListener listener = new PortalListener(this);
+    //private PPListenerv2 listener = new PPListenerv2(this); //New version
+    public final HashMap<Player, Location> lastposition = new HashMap<Player, Location>();
+    public PluginDescriptionFile pdf;
+
+    public void onDisable() {
+        System.out.println("[" + pdf.getName() + "] disabled!");
+    }
+
+    public void loadConfiguration() {
+        String limit = "border-limit";
+        getConfig().addDefault(limit, "50000000");
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    public void onEnable() {
+        loadConfiguration();
+        pdf = this.getDescription();
+        config = getConfig();
+        // Register our events
+        PluginManager pm = getServer().getPluginManager();
+        pm.registerEvents(listener, this);
+        // EXAMPLE: Custom code, here we just output some info so we can check all is well
+        getServer().getLogger().info(pdf.getName() + " version " + pdf.getVersion() + " is enabled!");
+        //System.out.println("["+pdf.getName()+"] portal distance limit: "+limit)
+
+    }
+}
