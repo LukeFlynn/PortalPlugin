4b384c4351055560cd811273ae7d896972a0b8aa
Luke Flynn <luke@qixalite.com>
2017-02-09 13:42:22 -0500

    Combine legacy and current versions of PortalPlugin per world

--- src/main/java/com/epochmc/portalplugin/PortalListener.java
+++ src/main/java/com/epochmc/portalplugin/PortalListener.java
@@ -23,18 +23,19 @@ import java.util.Arrays;
 public class PortalListener implements Listener {
 
     private final PortalPlugin plugin;
+    private LegacyPortalListener legListener;
 
     public PortalListener(PortalPlugin instance) {
         plugin = instance;
     }
 
     public int getLimit() {
-         int limit = plugin.config.getInt("border-limit", 2000);
-         return limit;
+        int limit = plugin.config.getInt("border-limit", 2000);
+        return limit;
     }
 
     public int getEncodeVal(int X, int Z, int Y, World w) {
-        Block block = w.getBlockAt(X, Y,Z);
+        Block block = w.getBlockAt(X, Y, Z);
         Material mat = block.getType();
         if (mat == Material.WOOL) {
             Wool wool = (Wool) block.getState().getData();
@@ -96,171 +97,176 @@ public class PortalListener implements Listener {
 
     @EventHandler
     public void onPlayerQuit(PlayerQuitEvent event) {
-        plugin.lastposition.remove(event.getPlayer());
+        if (!event.getPlayer().getWorld().getName().equals(plugin.getLegacyModeWorlds().getName())) {
+            plugin.lastposition.remove(event.getPlayer());
+        }
     }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
-        if (event.isCancelled()) {
-            return;
-        }
-        Location to = event.getTo();
-        Location lastPos = plugin.lastposition.get(event.getPlayer());
+        if (!event.getPlayer().getWorld().getName().equals(plugin.getLegacyModeWorlds().getName())) {
+            if (event.isCancelled()) {
+                return;
+            }
+            Location to = event.getTo();
+            Location lastPos = plugin.lastposition.get(event.getPlayer());
 
-        if (lastPos != null) {
-            //System.out.println("Player " + event.getPlayer().getName() + " moved to " + to);
-            if (Math.abs(to.getBlockX() - lastPos.getBlockX()) < 1) {
-                if (Math.abs(to.getBlockZ() - lastPos.getBlockZ()) < 1) {
-                    if (Math.abs(to.getBlockY() - lastPos.getBlockY()) < 1) {
-                        return;
+            if (lastPos != null) {
+                //System.out.println("Player " + event.getPlayer().getName() + " moved to " + to);
+                if (Math.abs(to.getBlockX() - lastPos.getBlockX()) < 1) {
+                    if (Math.abs(to.getBlockZ() - lastPos.getBlockZ()) < 1) {
+                        if (Math.abs(to.getBlockY() - lastPos.getBlockY()) < 1) {
+                            return;
+                        }
                     }
                 }
             }
-        }
-        World w = event.getPlayer().getWorld();
-        plugin.lastposition.put(event.getPlayer(), to);
+            World w = event.getPlayer().getWorld();
+            plugin.lastposition.put(event.getPlayer(), to);
 
-        int modX = (int) (to.getX() < 0 ? to.getX() - 1 : to.getX());
-        int modZ = (int) (to.getZ() < 0 ? to.getZ() - 1 : to.getZ());
+            int modX = (int) (to.getX() < 0 ? to.getX() - 1 : to.getX());
+            int modZ = (int) (to.getZ() < 0 ? to.getZ() - 1 : to.getZ());
 
-        Material block = w.getBlockAt(modX, (int) to.getY(), modZ).getType();
-        Material anchor = Material.SPONGE;
-        Material netherPortal = Material.PORTAL;
+            Material block = w.getBlockAt(modX, (int) to.getY(), modZ).getType();
+            Material anchor = Material.SPONGE;
+            Material netherPortal = Material.PORTAL;
 
-        if (block.equals(Material.STONE_PLATE) || (block.equals(Material.WOOD_PLATE)) || (block.equals(Material.ENDER_PORTAL)) || (block.equals(Material.PORTAL))) {
+            if (block.equals(Material.STONE_PLATE) || (block.equals(Material.WOOD_PLATE)) || (block.equals(Material.ENDER_PORTAL)) || (block.equals(Material.PORTAL))) {
 
-            int orientationlevel = (int) to.getY() - 1;
+                int orientationlevel = (int) to.getY() - 1;
 
-            //Change elevation for nether portal
-            int readdirection = -1; //0 is for +x 1 is +z 2 is -x 3 is -z
-            if (block.equals(netherPortal)) {
-                orientationlevel = (int) to.getY() - 2;
-            }
-
-            if (readdirection == -1) {
-                if (w.getBlockAt(modX - 1, orientationlevel, modZ).getType().equals(anchor)) {
-                    readdirection = 0;
+                //Change elevation for nether portal
+                int readdirection = -1; //0 is for +x 1 is +z 2 is -x 3 is -z
+                if (block.equals(netherPortal)) {
+                    orientationlevel = (int) to.getY() - 2;
                 }
-                if (w.getBlockAt(modX + 1, orientationlevel, modZ).getType().equals(anchor)) {
-                    readdirection = 2;
+
+                if (readdirection == -1) {
+                    if (w.getBlockAt(modX - 1, orientationlevel, modZ).getType().equals(anchor)) {
+                        readdirection = 0;
+                    }
+                    if (w.getBlockAt(modX + 1, orientationlevel, modZ).getType().equals(anchor)) {
+                        readdirection = 2;
+                    }
+                    if (w.getBlockAt(modX, orientationlevel, modZ + 1).getType().equals(anchor)) {
+                        readdirection = 3;
+                    }
+                    if (w.getBlockAt(modX, orientationlevel, modZ - 1).getType().equals(anchor)) {
+                        readdirection = 1;
+                    }
                 }
-                if (w.getBlockAt(modX, orientationlevel, modZ + 1).getType().equals(anchor)) {
-                    readdirection = 3;
-                }
-                if (w.getBlockAt(modX, orientationlevel, modZ - 1).getType().equals(anchor)) {
-                    readdirection = 1;
-                }
-            }
 
 
-            if (readdirection == -1) {
-                return;
-            } else {
-                float rotation = 0;
-                int xcodenum = 0;
-                int goto1 = 0;
-                int goto2 = 0;
-                int gotoY = 126;
-                switch (readdirection) {
-                    case -1:
-                        return;
-                    case 0:
-                        goto1 = -readColumn(modX + 1, modZ, orientationlevel - 1, 4, w);
-                        rotation = readColumn(modX - 1, modZ, orientationlevel - 3, 1, w) * 45;
-                        xcodenum = 1;
-                        if (w.getBlockAt(modX - 1, orientationlevel - 4, modZ).getType().equals(anchor)) {
-                            goto2 = -readColumn(modX, modZ, orientationlevel - 1, 4, w);
-                        } else {
-                            goto2 = readColumn(modX, modZ, orientationlevel - 1, 4, w);
-                        }
-
-                        gotoY = 255 - readColumn(modX - 1, modZ, orientationlevel - 1, 2, w);
-                        break;
-                    case 1:
-                        goto1 = -readColumn(modX, modZ + 1, orientationlevel - 1, 4, w);
-                        rotation = readColumn(modX, modZ - 1, orientationlevel - 3, 1, w) * 45;
-                        xcodenum = 2;
-                        if (w.getBlockAt(modX, orientationlevel - 4, modZ - 1).getType().equals(anchor)) {
-                            goto2 = -readColumn(modX, modZ, orientationlevel - 1, 4, w);
-                        } else {
-                            goto2 = readColumn(modX, modZ, orientationlevel - 1, 4, w);
-                        }
-
-                        gotoY = 255 - readColumn(modX, modZ - 1, orientationlevel - 1, 2, w);
-                        break;
-                    case 2:
-                        goto1 = readColumn(modX - 1, modZ, orientationlevel - 1, 4, w);
-                        rotation = readColumn(modX + 1, modZ, orientationlevel - 3, 1, w) * 45;
-                        xcodenum = 1;
-                        if (w.getBlockAt(modX + 1, orientationlevel - 4, modZ).getType().equals(anchor)) {
-                            goto2 = readColumn(modX, modZ, orientationlevel - 1, 4, w);
-                        } else {
-                            goto2 = -readColumn(modX, modZ, orientationlevel - 1, 4, w);
-                        }
-
-                        gotoY = 255 - readColumn(modX + 1, modZ, orientationlevel - 1, 2, w);
-                        break;
-                    case 3:
-                        goto1 = readColumn(modX, modZ - 1, orientationlevel - 1, 4, w);
-                        rotation = readColumn(modX, modZ + 1, orientationlevel - 3, 1, w) * 45;
-                        xcodenum = 2;
-                        if (w.getBlockAt(modX, orientationlevel - 4, modZ + 1).getType().equals(anchor)) {
-                            goto2 = readColumn(modX, modZ, orientationlevel - 1, 4, w);
-                        } else {
-                            goto2 = -readColumn(modX, modZ, orientationlevel - 1, 4, w);
-                        }
-
-                        gotoY = 255 - readColumn(modX, modZ + 1, orientationlevel - 1, 2, w);
-                        break;
-                }
-                Location loc = new Location(w, 0, 0, 0);
-                if (xcodenum == 1) {
-                    loc.setX(modX + goto1);
-                    loc.setZ(modZ + goto2);
-                }
-                if (xcodenum == 2) {
-                    loc.setX(modX + goto2);
-                    loc.setZ(modZ + goto1);
-                }
-                if (gotoY <= 0) {
-                    gotoY = 2;
-                }
-                loc.setY(gotoY);
-                if (rotation < 360) {
-                    loc.setYaw(event.getPlayer().getLocation().getYaw() + rotation);
+                if (readdirection == -1) {
+                    return;
                 } else {
-                    loc.setYaw(rotation);
-                }
-                loc.setPitch(event.getPlayer().getLocation().getPitch());
+                    float rotation = 0;
+                    int xcodenum = 0;
+                    int goto1 = 0;
+                    int goto2 = 0;
+                    int gotoY = 126;
+                    switch (readdirection) {
+                        case -1:
+                            return;
+                        case 0:
+                            goto1 = -readColumn(modX + 1, modZ, orientationlevel - 1, 4, w);
+                            rotation = readColumn(modX - 1, modZ, orientationlevel - 3, 1, w) * 45;
+                            xcodenum = 1;
+                            if (w.getBlockAt(modX - 1, orientationlevel - 4, modZ).getType().equals(anchor)) {
+                                goto2 = -readColumn(modX, modZ, orientationlevel - 1, 4, w);
+                            } else {
+                                goto2 = readColumn(modX, modZ, orientationlevel - 1, 4, w);
+                            }
 
-                //if(loc.x < 0)
-                loc.setX(loc.getX() + 0.5f);
-                //else
-                //	loc.x -= 0.5f;
-                //if(loc.z < 0)
-                loc.setZ(loc.getZ() + 0.5f);
-                //else
-                //	loc.z += 0.5f;
+                            gotoY = 255 - readColumn(modX - 1, modZ, orientationlevel - 1, 2, w);
+                            break;
+                        case 1:
+                            goto1 = -readColumn(modX, modZ + 1, orientationlevel - 1, 4, w);
+                            rotation = readColumn(modX, modZ - 1, orientationlevel - 3, 1, w) * 45;
+                            xcodenum = 2;
+                            if (w.getBlockAt(modX, orientationlevel - 4, modZ - 1).getType().equals(anchor)) {
+                                goto2 = -readColumn(modX, modZ, orientationlevel - 1, 4, w);
+                            } else {
+                                goto2 = readColumn(modX, modZ, orientationlevel - 1, 4, w);
+                            }
+
+                            gotoY = 255 - readColumn(modX, modZ - 1, orientationlevel - 1, 2, w);
+                            break;
+                        case 2:
+                            goto1 = readColumn(modX - 1, modZ, orientationlevel - 1, 4, w);
+                            rotation = readColumn(modX + 1, modZ, orientationlevel - 3, 1, w) * 45;
+                            xcodenum = 1;
+                            if (w.getBlockAt(modX + 1, orientationlevel - 4, modZ).getType().equals(anchor)) {
+                                goto2 = readColumn(modX, modZ, orientationlevel - 1, 4, w);
+                            } else {
+                                goto2 = -readColumn(modX, modZ, orientationlevel - 1, 4, w);
+                            }
+
+                            gotoY = 255 - readColumn(modX + 1, modZ, orientationlevel - 1, 2, w);
+                            break;
+                        case 3:
+                            goto1 = readColumn(modX, modZ - 1, orientationlevel - 1, 4, w);
+                            rotation = readColumn(modX, modZ + 1, orientationlevel - 3, 1, w) * 45;
+                            xcodenum = 2;
+                            if (w.getBlockAt(modX, orientationlevel - 4, modZ + 1).getType().equals(anchor)) {
+                                goto2 = readColumn(modX, modZ, orientationlevel - 1, 4, w);
+                            } else {
+                                goto2 = -readColumn(modX, modZ, orientationlevel - 1, 4, w);
+                            }
+
+                            gotoY = 255 - readColumn(modX, modZ + 1, orientationlevel - 1, 2, w);
+                            break;
+                    }
+                    Location loc = new Location(w, 0, 0, 0);
+                    if (xcodenum == 1) {
+                        loc.setX(modX + goto1);
+                        loc.setZ(modZ + goto2);
+                    }
+                    if (xcodenum == 2) {
+                        loc.setX(modX + goto2);
+                        loc.setZ(modZ + goto1);
+                    }
+                    if (gotoY <= 0) {
+                        gotoY = 2;
+                    }
+                    loc.setY(gotoY);
+                    if (rotation < 360) {
+                        loc.setYaw(event.getPlayer().getLocation().getYaw() + rotation);
+                    } else {
+                        loc.setYaw(rotation);
+                    }
+                    loc.setPitch(event.getPlayer().getLocation().getPitch());
+
+                    //if(loc.x < 0)
+                    loc.setX(loc.getX() + 0.5f);
+                    //else
+                    //	loc.x -= 0.5f;
+                    //if(loc.z < 0)
+                    loc.setZ(loc.getZ() + 0.5f);
+                    //else
+                    //	loc.z += 0.5f;
 
 
 
-                int limit = getLimit();
-                Location center = null;
-                if (plugin.config.getBoolean("center-origin", false)) {
-                    center = new Location(event.getPlayer().getWorld(), 0, 0, 0);
-                } else {
-                    center = event.getPlayer().getWorld().getSpawnLocation();
-                }
-                if (Math.abs(loc.getX() - center.getX()) <= limit
-                        && Math.abs(loc.getZ() - center.getZ()) <= limit) {
-                    event.getPlayer().sendMessage(ChatColor.DARK_GRAY + "Teleporting to " + loc.getX() + " " + loc.getY() + " " + loc.getZ());
-                    System.out.println("[" + plugin.pdf.getName() + "] Teleporting " + event.getPlayer().getName() +  " to " + loc.getX() + " " + loc.getY() + " " + loc.getZ());
-                    event.getPlayer().teleport(loc, TeleportCause.PLUGIN);
-                } else {
-                    event.getPlayer().sendMessage(ChatColor.RED + "Cannot Teleport you to " + loc.getX() + " "
-                            + loc.getY() + " " + loc.getZ());
+                    int limit = getLimit();
+                    Location center = null;
+                    if (plugin.config.getBoolean("center-origin", false)) {
+                        center = new Location(event.getPlayer().getWorld(), 0, 0, 0);
+                    } else {
+                        center = event.getPlayer().getWorld().getSpawnLocation();
+                    }
+                    if (Math.abs(loc.getX() - center.getX()) <= limit
+                            && Math.abs(loc.getZ() - center.getZ()) <= limit) {
+                        event.getPlayer().sendMessage(ChatColor.DARK_GRAY + "Teleporting to " + loc.getX() + " " + loc.getY() + " " + loc.getZ());
+                        System.out.println("[" + plugin.pdf.getName() + "] Teleporting " + event.getPlayer().getName() +  " to " + loc.getX() + " " + loc.getY() + " " + loc.getZ());
+                        event.getPlayer().teleport(loc, TeleportCause.PLUGIN);
+                    } else {
+                        event.getPlayer().sendMessage(ChatColor.RED + "Cannot Teleport you to " + loc.getX() + " "
+                                + loc.getY() + " " + loc.getZ());
+                    }
                 }
             }
+            return;
         }
         return;
     }
--- src/main/java/com/epochmc/portalplugin/PortalPlugin.java
+++ src/main/java/com/epochmc/portalplugin/PortalPlugin.java
@@ -1,6 +1,11 @@
 package com.epochmc.portalplugin;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -11,6 +16,7 @@ import org.bukkit.configuration.file.FileConfiguration;
 public class PortalPlugin extends JavaPlugin {
     public FileConfiguration config;
     private final PortalListener listener = new PortalListener(this);
+    private final LegacyPortalListener legListener = new LegacyPortalListener(this);
     public final HashMap<Player, Location> lastposition = new HashMap<Player, Location>();
     public PluginDescriptionFile pdf;
 
@@ -18,9 +24,23 @@ public class PortalPlugin extends JavaPlugin {
         System.out.println("[" + pdf.getName() + "] disabled!");
     }
 
+    public World getLegacyModeWorlds() {
+        List<String> worlds = config.getStringList("legacy-mode-worlds");
+        StringBuilder sbworld = new StringBuilder();
+
+        for (String world : worlds) {
+            sbworld.append(world);
+        }
+
+        World world = getServer().getWorld(sbworld.toString());
+        return world;
+    }
+
     public void loadConfiguration() {
         String limit = "border-limit";
+        String legacyMode = "legacy-mode-worlds";
         getConfig().addDefault(limit, 2000);
+        getConfig().addDefault(legacyMode, "[]");
         getConfig().options().copyDefaults(true);
         saveConfig();
     }
@@ -32,6 +52,13 @@ public class PortalPlugin extends JavaPlugin {
         // Register our events
         PluginManager pm = getServer().getPluginManager();
         pm.registerEvents(listener, this);
+        pm.registerEvents(legListener, this);
         getServer().getLogger().info(pdf.getName() + " version " + pdf.getVersion() + " is enabled! " + "Border Limit: " + listener.getLimit());
+        if (getServer().getWorlds().contains(getLegacyModeWorlds())) {
+            getServer().getLogger().info(pdf.getName() + " version " + pdf.getVersion() + " Legacy mode is enabled in worlds(s) " +  getLegacyModeWorlds().getName() + " Border Limit: " + legListener.getLimit());
+        } else {
+            getServer().getLogger().info(pdf.getName() + " version " + pdf.getVersion() + " Did not find any world(s) (" + getLegacyModeWorlds() + ") eligible for legacy mode!");
+        }
+
     }
 }
